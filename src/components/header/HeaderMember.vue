07361189bc04c7af5ce17764ff565fa98f219669
KSJ0314 <sojung017@naver.com>
2025-05-15 23:31:36 +0900

    fix : 다른 페이지에서 프로필 버튼 클릭시 오류 해결
    - 헤더의 프로필 아이콘이 a태그로 감싸져 있어서 메인으로 돌아오는 문제 발생
    - a태그 삭제

--- src/components/header/HeaderMember.vue
+++ src/components/header/HeaderMember.vue
@@ -4,18 +4,16 @@ import defaultProfile from '@/assets/img/default_profile.png'
 
 // pinia
 import { useSideStore } from '@/stores/side'
-const store = useSideStore()
+const sideStore = useSideStore()
 
 import { useLoginUserStore } from '@/stores/loginUser'
-const loginUser = useLoginUserStore()
+const loginUserStore = useLoginUserStore()
 </script>
 
 <template>
   <div class="member">
     <template v-if="auth.isLoggedIn()">
-      <a href="#" @click="store.myToggle(!store.my)"
-        ><img class="profile" :src="loginUser.profileImg || defaultProfile"
-      /></a>
+      <img @click="sideStore.myToggle(!sideStore.my)" class="profile" :src="loginUserStore.profileImg || defaultProfile"/>
     </template>
     <template v-else>
       <router-link to="/member/login" class="link">로그인</router-link>
@@ -41,6 +39,7 @@ const loginUser = useLoginUserStore()
   width: 40px;
   border-radius: 50%;
   overflow: hidden;
+  cursor: pointer;
 }
 .link {
   font-size: 0.8em;
