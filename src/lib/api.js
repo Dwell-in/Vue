2d5c4a62b2ceb52e36b2e0deae81c999de042dbe
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-05-22 17:55:19 +0900

    11

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_ROOT,
   baseURL: 'http://localhost:8081/',
-  // baseURL: 'https://dwell-in.onrender.com/',
+  // baseURL: '13.239.134.20:8081',
+  // baseURL: 'http://dwellin.store/',
   withCredentials: true, // 쿠키 전송 등 필요한 옵션
 })
 
